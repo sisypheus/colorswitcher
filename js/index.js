ca29e38180394621204ad7db02c188e5b2f5dd0f
tpoette <theo.poette@epitech.eu>
2020-07-27 22:23:31 +0200

    [+] randomize on button press

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
-document.body.onload = changebgcolor;
-
 function changebgcolor() {
     let bg = document.getElementsByClassName('bg')[0];
     let color = Math.floor(Math.random()*16777215).toString(16);
 
     let str = '#';
 
+    let span = document.getElementById('color');
+
+    span.innerHTML = color;
+
     let randcolor = str + color;
-    console.log(randcolor);
     bg.style.backgroundColor = randcolor;
 }
